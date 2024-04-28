d5a7e58a2b96f8d01a4292c08e8cd7a33fd7ddba
Singhscientist1 <Prashantgaharwar831@gmail.com>
2024-04-28 06:46:33 -0400

    added calculator

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask,request,render_template
-app = Flask (__name__)
-@app.route('/')
-def hello ():
-    return "Hello,World"
-print(__name__)
+from flask import Flask, request, jsonify
 
-if __name__=='__main__':
-    app.run(debug=True)
+obj = Flask(__name__)
+
+@obj.route('/')
+def hello():
+    return "Hello, World!"
+
+@obj.route('/cal', methods=["POST"])
+@obj.route('/cal', methods=["GET"])
+def math_operator():
+    data = request.get_json()
+    operation = data["operation"]
+    number1 = data["number1"]
+    number2 = data["number2"]
+    
+    if operation == "add":
+        result = int(number1) + int(number2)
+    elif operation == "multiply":
+        result = int(number1) * int(number2)
+    elif operation == "division":
+        result = int(number1) / int(number2)
+    else:
+        result = int(number1) - int(number2)
+        
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    obj.run(debug=True)
